afef3dd3f8170ac86664368ac61fc991bbf9cc76
guthulagoutham75-cmyk <guthulagoutham75@gmail.com>
2026-09-21 14:23:30 +0530

    Add files via upload

---
+++ Momo.java
@@ -0,0 +1,11 @@
+class Momo{
+    static int a;
+    static float b;
+    static short c;
+    static boolean d;
+    static double e;
+    static long f;
+    public static void main(String []args){
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f);
+    }
+}
